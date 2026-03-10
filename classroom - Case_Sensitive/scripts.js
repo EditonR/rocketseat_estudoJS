0815543be9bc38f11dedf87d784d8248b7ae854b
EditonR <editonrenne1997@hotmail.com>
2026-03-10 16:36:54 -0300

    classroom_case-sensitive

---
+++ classroom - Case_Sensitive/scripts.js
@@ -0,0 +1,15 @@
+/*
+Quando uma linguagem de programação é case-sensitive significa que ela é sensível a letras maiúsculas e minúsculas.
+
+Por exemplo: 'Rodrigo' é considerado diferente de 'rodrigo'
+
+
+*/
+
+var product = 'Teclado Mecânico'
+var Product = 'Mouse Gamer'
+
+var product = 'Fone sem fio'
+
+console.log(Product)
+console.log(product)
