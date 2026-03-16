3779a3161538f6ec48779414b605d509d6b1ca3b
EditonR <editonrenne1997@hotmail.com>
2026-03-16 20:34:16 -0300

    classroom - String

---
+++ classroom - String/scripts.js
@@ -0,0 +1,19 @@
+// String
+
+let userName = 'Eddie'
+console.log(userName)
+console.log (typeof userName)
+
+console.log("Uma String com aspas")
+console.log('Uma string com apóstrofo (aspas simples)')
+
+// Quando utilizar uma ou outra?
+
+console.log('Uma string com "aspas duplas" dentro de simples.') // Quando eu quero usar aspas duplas dentro do texto, eu uso aspas simples.
+
+console.log("Uma string com 'aspas simples' dentro de dupla")
+
+
+console.log(`
+  Uma string com acento grave permite escrever mútiplas linhas
+  `)
