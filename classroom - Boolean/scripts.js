f9e6c6341c19b6cc9b9a0699421143f54b637531
EditonR <editonrenne1997@hotmail.com>
2026-03-16 20:52:25 -0300

    classroom - Boolean

---
+++ classroom - Boolean/scripts.js
@@ -0,0 +1,10 @@
+// Boolean - guarda verdadeiro ou falso
+
+console.log(true)
+console.log(false)
+
+let isLoading = true
+
+console.log( typeof isLoading)
+
+//Tem este nome por causa do matemático George Boole
