4d9265a9ee010183add80b3f357bd136df832d55
EditonR <editonrenne1997@hotmail.com>
2026-03-16 20:49:09 -0300

    classroom - Number

---
+++ classroom - Number/scripts.js
@@ -0,0 +1,17 @@
+// Number
+console.log(typeof 5)
+
+//Inteiro positivo.
+console.log(5)
+
+//Inteiro negativo.
+console.log(-5)
+
+//Número reais ou float
+console.log(7.5)
+
+
+//Nan - Not a Number
+console.log(12.5 / "eddie")
+
+
