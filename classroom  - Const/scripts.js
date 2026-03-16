3a0a7fd6c00f8c364d05bc71b5c15c64ae4f1f11
EditonR <editonrenne1997@hotmail.com>
2026-03-16 19:27:32 -0300

    classroom - Const

--- classroom  - Const/scripts.js
+++ classroom  - Const/scripts.js
@@ -1,1 +1,17 @@
+/*
+Quando precisamos armazenar um valor que não deve ser alterado, podemos usar uma constante em JavaScript. Assim como uma variável, uma constante guarda um valor temporariamente na memória, porém esse valor é fixo e não pode ser modificado. Ao tentar alterar o valor de uma constante, um erro será gerado. É útil utilizar constantes quando queremos garantir que um valor não seja alterado. Essa prática será explorada ao longo das aulas, principalmente durante o desenvolvimento de projetos.
 
+*/
+
+//Uma constante cria uma variável que cujo valor é fixo(não pode ser alterado)
+const number = 43
+console.log(number)
+
+
+/*
+
+Gera um erro porque o valor não pode ser alterado.
+
+number = 55
+console.log(number)
+* */
