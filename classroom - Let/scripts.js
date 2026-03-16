e789a3ebfb2b3e223c590169dca52d3c6922addb
EditonR <editonrenne1997@hotmail.com>
2026-03-16 19:21:25 -0300

    classroom_let

---
+++ classroom - Let/scripts.js
@@ -0,0 +1,17 @@
+// Declarar uma variável sem valor.
+
+/*
+A diferença principal em relação ao "var" é que o "let" não permite a redeclaração de variáveis com o mesmo nome, evitando sobrescrita de conteúdo. Além disso, foi explicado como as variáveis funcionam na memória, sendo um espaço reservado com um nome como referência. O "let" é uma palavra reservada do JavaScript para criar variáveis.
+
+*/
+let user
+
+console.log('Editon Renne')
+
+
+//Declarar uma variável com valor.
+
+let email= 'editon@email.com.br'
+email = 'eddie@eddie.com'
+console.log(email)
+
