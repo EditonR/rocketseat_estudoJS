d8e37741d0c6ec9eb40243efe8dbb9b14b841566
EditonR <editonrenne1997@hotmail.com>
2026-03-16 20:08:58 -0300

    classroom - Nomes para variáveis

---
+++ classroom - Nome para variáveis/scripts.js
@@ -0,0 +1,52 @@
+// case-sensitive
+
+let username = 'Eddie'
+let userName = 'Kay'
+
+console.log(username)
+console.log(userName)
+
+
+//PODEMOS
+
+let $email = 'eddie@email.com'
+let _email = 'eddie@teste.com'
+let user_email ='mimi@teste.com'
+
+
+let Ação = 'Cadastrar' // Não é recomendado
+
+console.log($email)
+console.log(_email)
+console.log(Ação)
+console.log(user_email)
+
+
+let register = 'usuário x' // sempre escrever as variáveis/ funcções em inglês
+console.log(register)
+
+// NÃO PODEMOS
+//let 1user= 'Eddie'
+// criar variável com espaço
+
+//RECOMENDAção camelCase
+
+let productName ='Barra de chocolate'
+let firstName = 'Eddie'
+let lastName = 'Renne'
+
+//RECOMENDAÇÃO snake_case (rastejar)
+
+let product_name ='teclado'
+let first_name = 'Eddie'
+let last_name = 'Renne'
+
+
+/*
+abordamos a importância dos nomes de variáveis em JavaScript. Destaquei a sensibilidade a maiúsculas e minúsculas, a possibilidade de usar caracteres especiais e a recomendação de escrever em inglês para facilitar a leitura do código. Apresentei os padrões camelCase e snake_case, explicando como aplicá-los para criar nomes de variáveis mais legíveis. Reforcei a importância de escolher nomes significativos para as variáveis, facilitando a compreensão do código.
+
+*/
+
+
+
+
