0586c1c505b6e04fa8d806e5fcd7fa77a9c28ea2
EditonR <editonrenne1997@hotmail.com>
2026-03-16 20:55:42 -0300

    classroom - Undefined e Null

---
+++ classroom - Undefined e Null/scripts.js
@@ -0,0 +1,14 @@
+// undefined(indefinido) e null(nulo)
+
+let emptiness
+console.log("O valor é: ", emptiness)
+
+let empty = null
+console.log( "O valor é: ", empty)
+
+
+/*
+
+foi abordada a diferença entre Undefined e Null em JavaScript. Undefined é atribuído automaticamente a uma variável sem valor definido, enquanto Null é utilizado intencionalmente para indicar que a variável está vazia. Undefined é o valor padrão para uma variável sem conteúdo, enquanto Null é utilizado para explicitar a ausência de valor.
+
+*/
