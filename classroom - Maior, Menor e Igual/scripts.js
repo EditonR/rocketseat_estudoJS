ccadbc83fb235148a2667893451e2408d7199f34
EditonR <editonrenne1997@hotmail.com>
2026-03-18 19:26:30 -0300

    classroom - Maior, Menor e Igual

---
+++ classroom - Maior, Menor e Igual/scripts.js
@@ -0,0 +1,22 @@
+/*
+Nesta aula, expliquei sobre operadores de comparação em JavaScript. Demonstrei como usar os operadores de maior que, maior ou igual a, menor que e menor ou igual a para comparar valores. Utilizei um exemplo com variáveis de saldo e pagamento para mostrar como realizar essas comparações e obter resultados verdadeiros ou falsos. Esses operadores são úteis para verificar condições lógicas em um código.
+
+*/
+
+let balance = 500
+let payment = 120
+
+// Operador  > Maior que
+console.log(balance>payment)
+
+// Operador < Menor que
+console.log(balance<payment)
+
+
+// Operador >= Maior ou Igual a
+balance = 120
+console.log(balance>=payment)
+
+// Operador <= Maior ou Igual a
+balance = 500
+console.log(balance<=payment)
