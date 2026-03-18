03a165fb81ae5ca826b55bd571590781fb671e57
EditonR <editonrenne1997@hotmail.com>
2026-03-18 18:24:31 -0300

    classroom - Operadores Aritméticos

---
+++ classroom - Operadores Aritméticos/scripts.js
@@ -0,0 +1,23 @@
+
+/*
+abordamos os operadores aritméticos em JavaScript. Expliquei como usar o operador de soma, subtração, multiplicação, divisão, resto da divisão e exponenciação. Destaquei a diferença entre soma e concatenação, ressaltando a importância de usar tipos de dados numéricos para operações matemáticas. Demonstrei exemplos práticos de cada operador, enfatizando a utilidade do operador de resto da divisão para determinar se um número é par ou ímpar. Esses operadores são essenciais para realizar cálculos em JavaScript.
+*/
+
+//Operadores aritméticos
+
+console.log("SOMA: ", 12 + 8); // Soma é difente de concatenar.
+console.log("CONCATENAÇÃO: ", "12" + "8"); // Conctenação - Quando concatemos estamos efetuando uma união de texto.
+
+console.log("SUBTRAÇÃO: ", 12 - 8);
+
+console.log("MULTIPLICAÇÃO: ", 3 * 5.5)
+
+console.log("DIVISÃO: ", 12 / 2);
+
+console.log("RESTO DA DIVISÃO: ", 13 % 2); // Útil em alguns cenários como saber se o número é par ou ímpar.
+
+console.log("EXPONENCIAL/POTENCIA: ", 3 ** 3);
+
+// Operadores aritméticos que podemos utilizar para efetuar cálculos com o JavaScript
+
+
