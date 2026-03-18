3aaf93f32bbbf005d5a106f74660018ccb9cc79f
EditonR <editonrenne1997@hotmail.com>
2026-03-18 19:35:46 -0300

    classroom - Operadores de Atribuição

---
+++ classroom - Operadores de Atribuições/scripts.js
@@ -0,0 +1,42 @@
+/*
+Neste trecho, expliquei sobre os operadores de atribuição em JavaScript. Mostrei como usar o operador de igual para atribuir valores a variáveis, realizar incrementos, decrementos, multiplicação, divisão, resto da divisão e exponenciação. Demonstrei como esses operadores podem ser combinados com a atribuição de valores a variáveis. Ao longo da aula, exemplifiquei cada operação com console.log para visualização dos resultados.
+
+*/
+
+let value
+
+// Operador atribuição
+console.log("### ATRIBUÇÃO ###");
+value = 1
+console.log(value);
+
+// Incremento
+console.log("### INCREMENTO ###");
+value += 2
+console.log(value);
+
+//Decremento
+console.log("### DECREMENTO ###");
+value-=1
+console.log(value);
+
+//Multiplicar
+console.log("###MULTIPLICAÇÃO ###")
+value *= 3
+console.log(value)
+
+//Divisão
+console.log("### DIVISÃO ###")
+value /= 2
+console.log(value)
+
+//Resto da divisão
+console.log("### RESTO DA DIVISÃO ###")
+ //value %= 2
+ //console.log(value)
+
+ 
+//Potência
+console.log("### Potência ###")
+ value **= 2
+ console.log(value)
